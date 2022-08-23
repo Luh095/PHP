c2b141cd6a934b2eb6ca16bd8ac9e72b9a4a4dc6
Luana Roza <91756435+Luh095@users.noreply.github.com>
2022-08-23 16:30:33 -0300

    Add files via upload

---
+++ operadores/index.php
@@ -0,0 +1,27 @@
+<?php
+/* Operadores 
+-> São aqueles sinais que usamos para atribuir valores, fazer cálculos e muito mais...
+
+ex: + - * / = ++ -- += &&
+
+1) Operadores Aritméticos (matemáticos);
+2) Operadores de Atribuição;
+3) Operadores de Sequência (concatenação);
+4) Operadores de Comparação;
+5) Operador Condicional (ternário);
+6) Operadores Lógicos;
+
+São os mesmos do JavaScript;
+*/
+
+// javascript
+var nome = "Luana";
+var sobrenome = "Roza";
+
+var nome-completo = nome + sobrenome;
+
+// php
+$nome = "Luana";
+$sobrenome = "Roza";
+$nome_completo = $nome . $sobrenome;
+?>
